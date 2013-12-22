d43c3ef04e06188d53417733da21e140ed41c902
pataprout <alexandre@creakiwi.com>
2013-12-22 14:23:51 +0100

    [BUG] Missing required method in RandroidNotifier

--- src/com/creakiwi/randroid/RandroidNotifier.java
+++ src/com/creakiwi/randroid/RandroidNotifier.java
@@ -1,7 +1,15 @@
 package com.creakiwi.randroid;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
 
 public class RandroidNotifier extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
